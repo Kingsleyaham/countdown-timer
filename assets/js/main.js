0bd62637155056fe282d4ab480b1b1521afaddab
Aham Kingsley <kingsleyaham6@gmail.com>
2022-08-19 13:04:06 +0100

    made some few ammendment

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,22 +4,18 @@ function countdownTimer() {
   const minuteWrapper = document.getElementById("minute");
   const secondWrapper = document.getElementById("second");
 
-  let now = new Date();
-  let deadline = new Date("2022-09-02");
-  const difference = +deadline - +now; // Find the distance between now and deadline
+  let now = new Date().getTime();
+  let deadline = new Date("Sep 02, 2022 00:00:00").getTime();
 
-  if (difference < 0) {
-    daysWrapper.innerHTML = "00";
-    hourWrapper.innerHTML = "00";
-    minuteWrapper.innerHTML = "00";
-    secondWrapper.innerHTML = "00";
-  }
+  deadline =
+    deadline - now < 0 ? deadline + 1000 * 60 * 60 * 24 * 14 : deadline;
+  const distance = +deadline - +now; // Find the distance between now and deadline
 
   // Time calculations for days, hours, minutes and seconds
-  let days = Math.floor(difference / (1000 * 60 * 60 * 24));
-  let hours = Math.floor((difference / (1000 * 60 * 60)) % 24);
-  let minutes = Math.floor((difference / 1000 / 60) % 60);
-  let seconds = Math.floor((difference / 1000) % 60);
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // append 0 to days, hours, minutes, seconds if less than 10
   days = days < 10 ? `0${days}` : days;
