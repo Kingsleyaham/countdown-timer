92165d55845d9409fafdc25a66b305781208cfe3
Kingsleyaham <kingsleyaham6@gmail.com>
2024-12-13 11:36:53 +0100

    made some minor changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+const interval = setInterval(countdownTimer, 1000);
+
 function countdownTimer() {
   const daysWrapper = document.getElementById("days");
   const hourWrapper = document.getElementById("hour");
@@ -8,15 +10,14 @@ function countdownTimer() {
   let deadline = new Date("Dec 25, 2024 00:00:00").getTime();
 
   //   reset time if count down to 0
-  deadline =
-    deadline - now < 0 ? deadline + 1000 * 60 * 60 * 24 * 90 : deadline;
+  deadline = deadline - now < 0 ? deadline + 1000 * 60 * 60 * 24 * 90 : deadline;
   const distance = +deadline - +now; // Find the distance between now and deadline
 
   // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // append 0 to days, hours, minutes, seconds if less than 10
   days = days < 10 ? `0${days}` : days;
@@ -28,8 +29,14 @@ function countdownTimer() {
   hourWrapper.innerHTML = hours;
   minuteWrapper.innerHTML = minutes;
   secondWrapper.innerHTML = seconds;
+
+  if (distance <= 0) {
+    clearInterval(interval);
+    daysWrapper.innerHTML = "00";
+    hourWrapper.innerHTML = "00";
+    minuteWrapper.innerHTML = "00";
+    secondWrapper.innerHTML = "00";
+  }
 }
 
 countdownTimer();
-
-setInterval(countdownTimer, 1000);
